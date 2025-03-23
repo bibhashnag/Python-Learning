d145e8435302582eef9a970505182cea4535116c
bibhashnag <bibhash.nag@outlook.com>
2025-03-22 20:02:34 -0700

    operators

---
+++ Introduction/Operators.py
@@ -0,0 +1,47 @@
+# # Working with Arithmetic and Assignment in python 
+
+# # Addition
+# a = 30
+# b = 75
+# c = a + b
+# print("Result of addition:", c)
+
+# # Subtraction
+# d = b - a 
+# print("Result of subtraction:", d)
+
+# # Multiplication
+# e = a * b
+# print("Result of Multiplication:", e)
+
+# # Division
+# f = b / a
+# print("The result of division:", f)
+
+# # Modulus (Remainder)
+# g = b % a
+# print("The remainder is:", g)
+
+# # Exponentiation
+# h = a ** 2
+# print(f"The exponentiation of {a} is:", h)
+
+# # Floor Division
+# i = b // a 
+# print(f"The result of floor division of {b} and {a} is:", i)
+
+# Working with comparision Operators
+a = 300
+b = 500
+
+if a == b:
+    print("a is equal to b")
+
+if a != b:
+    print("a is not equal to b")
+
+if a < b:
+    print("a is less than b")
+
+if a > b:
+    print("a is greater than b")
